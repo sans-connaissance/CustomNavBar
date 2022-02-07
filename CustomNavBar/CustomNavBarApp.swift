b98cd4124a7b75aa5674c28aa3436b13837e466c
sans-connaissance <davemalicke@gmail.com>
2022-02-07 12:39:34 -0500

    Initial Commit

---
+++ CustomNavBar/CustomNavBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomNavBarApp.swift
+//  CustomNavBar
+//
+//  Created by David Malicke on 2/7/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomNavBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
